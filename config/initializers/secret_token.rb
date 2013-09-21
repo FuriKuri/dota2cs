da68617e405216912b7e1aba7dcd12415bb016fe
Theo Pack <tf.pack@googlemail.com>
2013-09-21 18:26:20 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dota2CS::Application.config.secret_key_base = '6442f67ddd89bb9de772a64f5d7f49e9bcf56d2843e89ce63f6c711b7c9a937aa6fad10cfdcdb613354359bbea5d2cdbd85e9fcafe9f049162466ed9ee363642'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dota2CS::Application.config.session_store :cookie_store, key: '_Dota2CS_session'
